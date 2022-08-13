74d5bafa687c640fe15baace05a7eb2030f37bc1
FRomero93 <romero.ezequiel.38679@gmail.com>
2022-08-13 20:29:51 -0300

    primer commit

---
+++ index.js
@@ -0,0 +1,14 @@
+let nombre = prompt("Ingrese su Nombre");
+let apellido = prompt("Ingrese su Apellido");
+let edad = parseInt(prompt("Ingrese su Edad"));
+
+console.log("generando id usuario...");
+const idSucursal =  12;
+console.log("sucursal asignada : " + idSucursal);
+var usuario = nombre[0] + apellido[0] + edad + idSucursal;
+console.log("id usuario generado...");
+console.log("id usuario: " + usuario);
+
+alert("Le damos la bienvenida ¡" + nombre + " " + apellido + "!");
+alert("Su usuario es: " + usuario);
+
