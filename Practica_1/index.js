b7a38506ef804cbdaa6572dd98df81658739a2f1
FRomero93 <romero.ezequiel.38679@gmail.com>
2022-08-18 18:49:05 -0300

    Se suben validaciones condicionales

--- Practica_1/index.js
+++ Practica_1/index.js
@@ -1,17 +1,29 @@
 let nombre = prompt("Ingrese su Nombre");
 let apellido = prompt("Ingrese su Apellido");
-let edad = parseInt(prompt("Ingrese su Edad"));
+let edad = Number(prompt("Ingrese su Edad"));
 
-console.log("generando id usuario...");
-const idSucursal =  12;
-console.log("sucursal asignada : " + idSucursal);
-var usuario = nombre[0] + apellido[0] + edad + idSucursal;
-console.log("id usuario generado...");
-console.log("id usuario: " + usuario);
+if(!isNaN(edad)){
+    console.log("generando id usuario...");
+    const idSucursal =  12;
+    console.log("sucursal asignada : " + idSucursal);
+    var usuario = nombre[0].toUpperCase() + apellido[0].toUpperCase() + edad + idSucursal;
+    console.log("id usuario generado...");
+    console.log("id usuario: " + usuario);
 
-alert("Le damos la bienvenida ¡" + nombre + " " + apellido + "!");
-alert("Su usuario es: " + usuario);
+    alert("Le damos la bienvenida ¡" + nombre + " " + apellido + "!");
+    alert("Su usuario es:" + usuario);
 
-let direccion = prompt("Ingrese su direccion...");
-console.log("direccion guardada");
-
+    let vivienda = prompt("Ingrese en que tipo de vivienda vive (Ejem: Casa, Departamento, etc...)").toLowerCase();
+    console.log("Vivienda: " + vivienda);
+    if(vivienda == "casa"){
+        alert("Usted vive en una casa 🏠");
+    }else if(vivienda == "departamento"){
+        alert("Usted vive en una departamento 🏢");
+    }else if(vivienda == "castillo"){
+        alert("Usted vive en un castillo 🏰");
+    }else{
+        alert("Usted no tiene vivienda 🏕");
+    }
+}else{
+    alert("Debe ingresar una edad válida...");
+}
